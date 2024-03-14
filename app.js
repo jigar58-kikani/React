8711d715ed60d765defd5cec71347929e21c0277
jigar58 <jigarkikani24@gmail.com>
2024-03-14 11:33:33 +0530

    igniting app and learn about the parcel and other dependencies.

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+/*
+ Created a server
+ Parcel do HMR - Hot Module Replacement
+ File watcher algorithm
+ minify
+ bundling
+ cleaning our code
+ dev and Production build
+ super fast build algorithm
+ image optimization
+ caching while development
+ compressing the file
+ compatable with older version of the browser
+ https on dev
+ manage port numbers
+ Consistent hashing algorithm
+ zero config
+ */
+import React from "react";
+import ReactDOM from "react-dom/client"
 const heading = React.createElement("h1", 
 {
     id: "title"
 }, 
-"Heading1")
+"Heading1 for parcel")
 const heading2 = React.createElement("h2", 
 {
     id: "title"
